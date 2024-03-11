a80df1c0a1f8145f1fda256345397ce4cca5e010
Emily Chevy <142541332+EmChevy@users.noreply.github.com>
2024-03-11 21:39:02 +0000

    Mon, Mar 11, 2024, 2:39 PM -07:00

---
+++ js/landingPage.js
@@ -0,0 +1,13 @@
+toggle = document.getElementById('toggle')
+open = document.getElementById('open')
+modal = document.getElementById('model')
+
+
+toggle.addEventListener('click', () => {
+    document.body.classList.toggle('show-nav')
+})
+
+
+open.addEventListener('click', () => {
+    modal.classList.add('show-model')
+})
