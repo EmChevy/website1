8ac87f04c0148f834c3fc171078c9d128f7af62c
Emily Chevy <142541332+EmChevy@users.noreply.github.com>
2024-03-12 21:47:12 +0000

    Tue, Mar 12, 2024, 2:47 PM -07:00

--- js/index.js
+++
@@ -1,7 +0,0 @@
-toggle = document.getElementById('toggle')
-open = document.getElementById('open')
-close = document.getElementById('close')
-
-toggle.addEventListener('click', () => {
-    document.body.classList.toggle('show-pop')
-})
