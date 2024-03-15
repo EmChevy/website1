bdc07491e2e98965d7bc8685a9f3cff149863a3e
Emily Chevy <142541332+EmChevy@users.noreply.github.com>
2024-03-15 22:02:26 +0000

    Fri, Mar 15, 2024, 3:02 PM -07:00

--- js/index.js
+++ js/index.js
@@ -1,26 +1,9 @@
 toggle = document.getElementById('toggle')
-open = document.getElementById('open')
-modal = document.getElementById('modal')
-close = document.getElementById('close')
+
 
 
 toggle.addEventListener('click', () => {
     document.body.classList.toggle('show-modal')
 })
 
-// show Modal
-open.addEventListener('click', () => {
-    modal.classList.add('show-modal')
-})
 
-// hide Modal
-close.addEventListener('click', () => {
-    modal.classList.remove('show-modal')
-})
-
-// hide Modal on outside click
-window.addEventListener('click', (e) => {
-    if (e.target == modal) {
-        modal.classList.remove('show-modal')
-    }
-})
