8b17c156be672111a2b13e1fbe95fdccf8831f9b
Emily Chevy <142541332+EmChevy@users.noreply.github.com>
2024-03-19 15:02:33 -0700

    overflow change

--- js/index.js
+++ js/index.js
@@ -3,3 +3,24 @@ toggle = document.getElementById('toggle')
 toggle.addEventListener('click', () => {
     document.body.classList.toggle('show-modal')
 })
+
+// open = document.getElementById('open')
+// modal = document.getElementById('modal')
+// close = document.getElementById('close')
+
+// // show Modal
+// open.addEventListener('click', () => {
+//     modal.classList.add('show')
+// })
+
+// // hide Modal
+// close.addEventListener('click', () => {
+//     modal.classList.remove('show')
+// })
+
+// // hide Modal on outside click
+// window.addEventListener('click', (e) => {
+//     if (e.target == modal) {
+//         modal.classList.remove('show')
+//     }
+// })
